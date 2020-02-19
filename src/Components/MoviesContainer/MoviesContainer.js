908ea275383db7f8b725a08c3b8ce2a4c4354469
Veronica Andrade <veronica726@gmail.com>
2020-02-18 18:10:42 -0700

    Add a filtered list of movies based on rating

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './MoviesContainer.css';
 import MoviePreview from '../MoviePreview/MoviePreview';
 
 const MoviesContainer = ({ movies }) => {
-  return movies.map(movie => {
+  let allMovies =  movies.map(movie => {
     return <MoviePreview key={movie.id} movie={movie}/>
   })
+
+  let filteredMovies =  movies.filter(film => {
+    return film.average_rating > 5
+  })
+
+  let topMovies = filteredMovies.map(movie => {
+    return <MoviePreview key = {movie.id} movie = {movie}/>
+  })
+  
+      console.log(filteredMovies)
+  return (
+    <section className='movies-display-container'>
+      <section className='top-rated-movies'>
+        {topMovies}
+      </section>
+      <section className='bottom-display-section'>
+        {allMovies}
+      </section>
+    </section>
+  )
 }
 
 export default MoviesContainer;
