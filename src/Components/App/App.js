4850152508b8dc7eb2f69cdef73b870f559f816b
Carla Geglio <carla.geglio@gmail.com>
2020-02-19 20:11:07 -0700

    Adjust login style

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import LoginForm from '../LoginForm/LoginForm'
 import Nav from '../Nav/Nav';
 import './App.css';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -13,17 +14,27 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
+      .then(response => response.json())
+      .then(movies => this.setState({movies: movies.movies}))
+      .catch(error => console.log(error))
+  }
+
   render() {
     return(
        <main>
         <Route exact path="/">
           <Nav />
-          <MoviesContainer />
+          {this.state.movies.length ?
+          <MoviesContainer movies={this.state.movies}/>
+          : <p>Error</p>
+          }
         </Route>
         <Route exact path="/login">
           <section className="login-page">
             <h2 className='login-msg'>Get your ratings on.</h2>
-            <img src={process.env.PUBLIC_URL + '/images/clapper.png'} alt="Clapperboard icon" className="clapper" />
+            <img src={process.env.PUBLIC_URL + '/clapper.png'} alt="Clapperboard icon" className="clapper" />
             <LoginForm />
           </section>
         </Route>
