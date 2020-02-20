7d8e16f2138c8b5b3a6eb8951b2f76513c4ff48e
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-20 14:07:07 -0700

    Add average rating

--- src/Components/MoviePreview/MoviePreview.js
+++ src/Components/MoviePreview/MoviePreview.js
@@ -4,6 +4,7 @@ import './MoviePreview.css';
 const MoviePreview = ({ movie }) => {
   return (
     <article className='movie-preview-container'>
+      <p className='preview-avg-rating'>{movie.average_rating}</p>
       <img src={movie.poster_path} alt='movie poster' className='movie-poster-image'/>
     </article>
   )
