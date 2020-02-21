c857636a4a47887a24b4755d0a4a1c989ec4cf08
Carla Geglio <carla.geglio@gmail.com>
2020-02-20 19:39:06 -0700

    Add get user ratings action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { movies } from './moviesReducer'
 import { user } from './userReducer'
+import { ratings } from './ratingsReducer'
 
 export const rootReducer = combineReducers({
   movies,
-  user
+  user,
+  ratings
 });
