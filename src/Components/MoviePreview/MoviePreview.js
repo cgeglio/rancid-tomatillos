3f2010cb148fce9bc6596e2cfa067558457da4e5
Carla Geglio <carla.geglio@gmail.com>
2020-02-20 17:21:34 -0700

    Add method to format release date

--- src/Components/MoviePreview/MoviePreview.js
+++ src/Components/MoviePreview/MoviePreview.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './MoviePreview.css';
 
-const MoviePreview = ({ movie }) => {
+const MoviePreview = ({ date, movie }) => {
   return (
     <article className='movie-preview-container'>
+      <p>{date}</p>
       <p className='preview-avg-rating'>{movie.average_rating}</p>
       <img src={movie.poster_path} alt='movie poster' className='movie-poster-image'/>
     </article>
