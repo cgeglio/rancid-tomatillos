f9803e0aef8ad4df55b7d8cab76e995077cb0f11
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-21 18:31:38 -0700

    Refactor style

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,8 @@ import { Route, Redirect } from 'react-router-dom';
 import MoviesContainer from '../../containers/MoviesContainer/MoviesContainer';
 import LoginForm from '../../containers/LoginForm/LoginForm'
 import Nav from '../Nav/Nav';
+import MovieDetails from '../MovieDetails/MovieDetails';
+import Loading from '../Loading/Loading';
 import './App.css';
 import { connect } from 'react-redux'
 
@@ -12,22 +14,24 @@ class App extends Component {
   render() {
     return(
        <main>
-        <Route exact path="/">
-          <Nav />
-
-          <MoviesContainer />
-
-        </Route>
-        <Route exact path="/login">
-          {this.props.user.id ? <Redirect to="/" /> :
-            <section className="login-page">
-              <h2 className='login-msg'>Get your ratings on.</h2>
-              <img src={process.env.PUBLIC_URL + '/clapper.png'} alt="Clapperboard icon" className="clapper" />
-              <LoginForm />
-            </section>
-          }
-        </Route>
-      </main>
+       <Route exact path='/'>
+         <Nav />
+         <MoviesContainer />
+       </Route>
+       <Route exact path='/movie_details'>
+        <Nav />
+         <MovieDetails />
+       </Route>
+       <Route exact path='/login'>
+         {this.props.user.id ? <Redirect to='/' /> :
+           <section className='login-page'>
+             <h2 className='login-msg'>Get your ratings on.</h2>
+             <img src={process.env.PUBLIC_URL + '/clapper.png'} alt='Clapperboard icon' className='clapper' />
+             <LoginForm />
+           </section>
+         }
+       </Route>
+       </main>
     )
   }
 }
