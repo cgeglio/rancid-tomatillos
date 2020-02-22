422f295437aeab77e0fcdd2e359a725528714b2e
Carla Geglio <carla.geglio@gmail.com>
2020-02-21 20:18:58 -0700

    Add lemon icon to movie preview

--- src/Components/MoviePreview/MoviePreview.js
+++ src/Components/MoviePreview/MoviePreview.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './MoviePreview.css';
+import lemon from '../../Assets/lemon.png';
 
 const MoviePreview = ({ date, movie, saveSelectedMovieToStore  }) => {
   return (
     <article onClick={ () => saveSelectedMovieToStore(movie) } className='movie-preview-container'>
       <p>{date}</p>
       <div className='ratings'>
-        <p className='preview-rating'>{movie.average_rating}</p>
-        {movie.user_rating ? <p className='preview-rating'>{movie.user_rating}</p> : <button className='preview-rating-button'>Rate</button>}
+        <div className='rating-bundle'><p className='preview-rating'>{movie.average_rating}</p>
+        <img src={lemon} className='rating-lemon'/></div>
+        {movie.user_rating ? <div className='rating-bundle'><p className='preview-user-rating'>{movie.user_rating}</p><img src={lemon} className='rating-lemon'/></div> : <button className='preview-rating-button'>Rate</button>}
       </div>
       <Link to={`/movie_details`}><img src={movie.poster_path} alt='movie poster' className='movie-poster-image'/></Link>
     </article>
