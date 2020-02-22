2868cc6e756b464098d16fe33cbb4c76a3086584
Carla Geglio <carla.geglio@gmail.com>
2020-02-22 14:41:12 -0700

    Update movie details tests

--- src/containers/MovieDetails/MovieDetails.test.js
+++ src/containers/MovieDetails/MovieDetails.test.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MovieDetails from './MovieDetails';
+import { MovieDetails, mapStateToProps }from './MovieDetails';
 import { shallow } from 'enzyme';
 
 describe('MovieDetails', () => {
+  describe('MovieDetails container/component', () => {
+    it('should match the snapshot', () => {
+      const wrapper = shallow(<MovieDetails selectedMovie={{movie: {title: 'Sonic the Hedgehog', release_date: '2020-02-10'}}} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
 
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<MovieDetails />)
-  })
-
-  it ('should render a MovieDetails component', () => {
-    expect(wrapper).toMatchSnapshot()
-  })
-
-})
+  describe('mapStateToProps', () => {
+    it('should return an object with the selectedMovie information', () => {
+      const mockState = {
+        movies: [ { title: 'Jumanji' } ],
+        selectedMovieReducer: {movie: {title: 'Sonic the Hedgehog'}},
+        ratings: [{rating: 5}, {rating: 4}],
+      };
+      const expected = {
+        selectedMovie: {movie: {title: 'Sonic the Hedgehog'}},
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected)
+    });
+  });
+});
