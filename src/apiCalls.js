3850e9a3b170ab6c080fbe7e1cfeda271c4e6e52
Veronica Andrade <veronica726@gmail.com>
2020-02-22 12:41:25 -0700

    Extract fetch call for getting the users ratings

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,9 +8,19 @@ export const getUserInfo = user => {
   }
   return fetch('https://rancid-tomatillos.herokuapp.com/api/v1/login', options)
     .then(response => {
-      if (!response.ok) {
-        throw Error('200 status code not found: getUserInfo throw error')
-      }
-      return response.json()
-    })
+    if (!response.ok) {
+      throw Error('200 status code not found: getUserInfo throw error')
+    }
+    return response.json()
+  })
+}
+
+export const getUserRatings = userId => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings`)
+  .then(response => {
+    if (!response.ok) {
+      throw Error('200 status code not found: getUserRatings throw error')
+    }
+    return response.json()
+  })
 }
