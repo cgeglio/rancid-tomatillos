957f301a2313f6b7e678a838a446886b02f14d19
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-21 18:35:33 -0700

    Add tests

--- src/containers/MoviesContainer/MoviesContainer.js
+++ src/containers/MoviesContainer/MoviesContainer.js
@@ -3,16 +3,20 @@ import './MoviesContainer.css';
 import MoviePreview from '../../Components/MoviePreview/MoviePreview';
 import { connect } from 'react-redux';
 import { getMovies } from '../../actions'
+import { updateSelectedMovie } from '../../actions';
 
 class MoviesContainer extends Component {
 
   componentDidMount() {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
       .then(response => response.json())
-      .then(movies => {
-        console.log(movies.movies)
-        this.props.addMoviesToStore(movies.movies)})
+      .then(movies => this.props.addMoviesToStore(movies.movies))
       .catch(error => console.log(error))
+  }
+
+  saveSelectedMovieToStore = movie => {
+    this.props.addSelectedMovieToStore(movie)
+    console.log('in saveSelectedMovieToStore method', movie);
   }
 
   render() {
@@ -20,7 +24,7 @@ class MoviesContainer extends Component {
       return  <p>loading</p>
     }
     let allMovies = this.props.movies.map(movie => {
-      return <MoviePreview key={movie.id} movie={movie}/>})
+      return <MoviePreview saveSelectedMovieToStore={this.saveSelectedMovieToStore} key={movie.id} movie={movie}/>})
     return (
       <section className='movies-display-container'>
         <section className='top-rated-movies'>
@@ -34,11 +38,13 @@ class MoviesContainer extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  movies: state.movies
+  movies: state.movies,
+  selectedMovie: state.selectedMovieReducer
 })
 
 const mapDispatchToProps = (dispatch) =>({
-  addMoviesToStore: movies => dispatch(getMovies(movies))
+  addMoviesToStore: movies => dispatch(getMovies(movies)),
+  addSelectedMovieToStore: movie => dispatch(updateSelectedMovie(movie))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(MoviesContainer);
