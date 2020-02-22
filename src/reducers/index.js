5b5c18774a0647519acf766c08e22fb669d3ed75
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-21 18:36:00 -0700

    Add selectedMovieReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { movies } from './moviesReducer'
 import { user } from './userReducer'
+import { selectedMovieReducer } from './selectedMovieReducer';
 
 export const rootReducer = combineReducers({
+  selectedMovieReducer,
   movies,
   user
 });
