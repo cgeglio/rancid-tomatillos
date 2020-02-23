f2a99b342033a6fcc900d86be3f5af94d7438f6e
Carla Geglio <carla.geglio@gmail.com>
2020-02-23 14:17:00 -0700

    Update movie details tests

--- src/containers/MovieDetails/MovieDetails.test.js
+++ src/containers/MovieDetails/MovieDetails.test.js
@@ -6,20 +6,23 @@ import { shallow } from 'enzyme';
 describe('MovieDetails', () => {
   describe('MovieDetails container/component', () => {
     it('should match the snapshot', () => {
-      const wrapper = shallow(<MovieDetails selectedMovie={{movie: {title: 'Sonic the Hedgehog', release_date: '2020-02-10'}}} />);
+      const wrapper = shallow(<MovieDetails selectedMovie={{movie: {title: 'Sonic the Hedgehog', release_date: '2020-02-10', user_rating: 5, average_rating: 8}}} />);
       expect(wrapper).toMatchSnapshot();
     });
   });
 
   describe('mapStateToProps', () => {
-    it('should return an object with the selectedMovie information', () => {
+    it('should return an object with the selectedMovie information, the user, and the user\'s ratings', () => {
       const mockState = {
         movies: [ { title: 'Jumanji' } ],
-        selectedMovieReducer: {movie: {title: 'Sonic the Hedgehog'}},
+        selectedMovieReducer: {movie: {title: 'Sonic the Hedgehog', user_rating: 7, average_rating: 8}},
         ratings: [{rating: 5}, {rating: 4}],
+        user: {name: 'Eric, id: 30'},
       };
       const expected = {
-        selectedMovie: {movie: {title: 'Sonic the Hedgehog'}},
+        selectedMovie: {movie: {title: 'Sonic the Hedgehog', user_rating: 7, average_rating: 8}},
+        ratings: [{rating: 5}, {rating: 4}],
+        user: {name: 'Eric, id: 30'},
       }
       const mappedProps = mapStateToProps(mockState);
       expect(mappedProps).toEqual(expected)
