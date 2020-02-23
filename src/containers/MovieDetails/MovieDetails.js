888110baed102647459ab0070efbe3defd46fa06
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-22 18:07:40 -0700

    Add movie dropbox state to component

--- src/containers/MovieDetails/MovieDetails.js
+++ src/containers/MovieDetails/MovieDetails.js
@@ -4,11 +4,26 @@ import './MovieDetails.css';
 import { connect } from 'react-redux';
 
 export class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state =
+      {
+        ratingDropbox: null
+      }
+  }
 
   formatDate = (releaseDate) => {
      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let date = releaseDate.split('-')
     return `${monthNames[date[1] - 1]} ${date[2]}, ${date[0]}`;
+  }
+
+  updateRatingState = num => {
+    this.setState({ratingDropbox: parseInt(num)})
+  }
+
+  submitRating = (rating, userId, movieId) => {
+
   }
 
   render() {
@@ -21,10 +36,22 @@ export class MovieDetails extends Component {
           <p>{this.formatDate(this.props.selectedMovie.movie.release_date)}</p>
           <p className='movie-overview'>{this.props.selectedMovie.movie.overview}</p>
           <div className='rating-container'>
-            <button className='rating-button'>+</button>
-            <p className='movie-number'>{this.props.selectedMovie.movie.average_rating}</p>
-            <button className='rating-button'>-</button>
+            <select onChange={e => this.updateRatingState(e.target.value)} id='rating-dropbox'>
+              <option value={null}>rate!</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+              <option value={6}>6</option>
+              <option value={7}>7</option>
+              <option value={8}>8</option>
+              <option value={9}>9</option>
+              <option value={10}>10</option>
+            </select>
+            <button className='submit-rating-button'>submit rating</button>
           </div>
+          <p className='movie-number'>current rating: {this.props.selectedMovie.movie.average_rating}</p>
         </div>
       </article>
     )
