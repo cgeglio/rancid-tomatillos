e6440c30f718488b4e41326b7563aa80df8e2b76
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-23 12:24:16 -0700

    Fix rating logic

--- src/containers/MovieDetails/MovieDetails.js
+++ src/containers/MovieDetails/MovieDetails.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './MovieDetails.css';
 import { connect } from 'react-redux';
+import { getRatings } from '../../actions';
 
 export class MovieDetails extends Component {
-  constructor() {
-    super();
-    this.state =
-      {
-        ratingDropbox: null
-      }
+  constructor(props) {
+    super(props);
+    this.state = {ratingDropbox: null}
   }
 
   formatDate = (releaseDate) => {
-     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     let date = releaseDate.split('-')
     return `${monthNames[date[1] - 1]} ${date[2]}, ${date[0]}`;
   }
@@ -22,8 +20,63 @@ export class MovieDetails extends Component {
     this.setState({ratingDropbox: parseInt(num)})
   }
 
-  submitRating = (rating, userId, movieId) => {
+  submitRating = (userId, movieId, rating) => {
+    if (!this.state.ratingDropbox) {
+      window.alert(`Please select a rating number to submit!`)
+    } else {
+      const ratingId = this.findMovieRatingId(movieId)
+      if (!ratingId) {
+        this.makeRatingRequest(userId, movieId, rating )
+      } else {
+        this.makeDeleteRequest(userId, ratingId, movieId, rating)
+      }
+    }
+  }
 
+  makeRatingRequest = (userId, movieId, rating) => {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        movie_id: movieId,
+        rating: rating
+        }),
+      headers: {'Content-Type': 'application/json'}
+    }
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings`, options)
+      .then(res => {
+      if(!res.ok) {
+        throw Error('Something is not right, try again later')
+      }
+      return res.json()})
+  }
+
+  makeDeleteRequest = (userId, ratingId, movieId, rating) => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings/${ratingId}`, {
+    method: "DELETE",
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Error Message');
+      }
+    })
+    .then( () => this.getUserRatings(userId, movieId, rating ))
+  }
+
+  getUserRatings = (userId, movieId, rating ) => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v1/users/${userId}/ratings`)
+      .then(response => response.json())
+      .then(ratings => this.props.addUserRatings(ratings.ratings))
+      .then(() => this.makeRatingRequest(userId, movieId, rating ))
+      .catch(error => console.log(error))
+  }
+
+  findMovieRatingId = id => {
+    let ratingIdNum = this.props.ratings.filter(movie => movie.movie_id === id)
+    if (ratingIdNum.length) {
+      return ratingIdNum[0].id
+    } else {
+      return 0;
+    }
   }
 
   render() {
@@ -49,9 +102,9 @@ export class MovieDetails extends Component {
               <option value={9}>9</option>
               <option value={10}>10</option>
             </select>
-            <button className='submit-rating-button'>submit rating</button>
+            <button className='submit-rating-button' onClick={() => this.submitRating(this.props.user.id, this.props.selectedMovie.movie.id, this.state.ratingDropbox)}>submit rating</button>
           </div>
-          <p className='movie-number'>current rating: {this.props.selectedMovie.movie.average_rating}</p>
+          <p className='movie-number'>average rating: {this.props.selectedMovie.movie.average_rating}</p>
         </div>
       </article>
     )
@@ -59,7 +112,13 @@ export class MovieDetails extends Component {
 }
 
 export const mapStateToProps = (state) => ({
-  selectedMovie: state.selectedMovieReducer
+  selectedMovie: state.selectedMovieReducer,
+  user: state.user,
+  ratings: state.ratings
 })
 
-export default connect(mapStateToProps)(MovieDetails);
+export const mapDispatchToProps = dispatch => ({
+  addUserRatings: ratings => dispatch(getRatings(ratings))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetails);
