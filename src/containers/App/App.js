dc779cb611baa4a03190aa431cb47f0bc43d201b
Carla Geglio <carla.geglio@gmail.com>
2020-02-23 19:18:12 -0700

    Add switch to redirect to homepage

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import MoviesContainer from '../../containers/MoviesContainer/MoviesContainer';
 import LoginForm from '../../containers/LoginForm/LoginForm'
 import Nav from '../Nav/Nav';
@@ -13,28 +13,31 @@ export class App extends Component {
   render() {
     return(
        <main>
-       <Route exact path='/'>
-         <Nav />
-         <MoviesContainer />
-       </Route>
-       <Route path='/movies/:id'>
-          {!this.props.user.id ? <Redirect to="/" /> : (
-          <>
+       <Switch>
+         <Route exact path='/'>
            <Nav />
-           <MovieDetails />
-          </>
-         )}
-       </Route>
-       <Route exact path='/login'>
-         {this.props.user.id ? <Redirect to='/' /> :
-           <section className='login-page'>
-             <h2 className='login-msg'>Get your ratings on.</h2>
-             <img src={process.env.PUBLIC_URL + '/clapper.png'} alt='Clapperboard icon' className='clapper' />
-             <LoginForm />
-           </section>
-         }
-       </Route>
-       </main>
+           <MoviesContainer />
+         </Route>
+         <Route path='/movies/:id'>
+            {!this.props.user.id ? <Redirect to="/" /> : (
+            <>
+             <Nav />
+             <MovieDetails />
+            </>
+           )}
+         </Route>
+         <Route exact path='/login'>
+           {this.props.user.id ? <Redirect to='/' /> :
+             <section className='login-page'>
+               <h2 className='login-msg'>Get your ratings on.</h2>
+               <img src={process.env.PUBLIC_URL + '/clapper.png'} alt='Clapperboard icon' className='clapper' />
+               <LoginForm />
+             </section>
+           }
+         </Route>
+         <Redirect to='/' />
+       </Switch>
+      </main>
     )
   }
 }
